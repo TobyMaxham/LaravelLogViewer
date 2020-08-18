296b86c462dd6533aa9b8117423bae53e0261ea8
TobyMaxham <git2019@maxham.de>
2020-08-18 11:42:54 +0200

    added some laravel 7 changes

--- src/LogViewerController.php
+++ src/LogViewerController.php
@@ -2,12 +2,20 @@
 
 namespace TobyMaxham\Logger;
 
-use Illuminate\Routing\Controller;
+if (class_exists("\\Illuminate\\Routing\\Controller")) {
+    class BaseController extends \Illuminate\Routing\Controller
+    {
+    }
+} elseif (class_exists("Laravel\\Lumen\\Routing\\Controller")) {
+    class BaseController extends \Laravel\Lumen\Routing\Controller
+    {
+    }
+}
 
 /**
  * @author Tobias Maxham <git2020@maxham.de>
  */
-class LogViewerController extends Controller
+class LogViewerController extends BaseController
 {
 
     /**
@@ -16,34 +24,42 @@ class LogViewerController extends Controller
     private $log_viewer;
 
     /**
+     * @var \Illuminate\Contracts\Foundation\Application|\Illuminate\Foundation\Application|mixed
+     */
+    private $request;
+
+    /**
      * LogViewerController constructor.
      */
     public function __construct()
     {
         $this->log_viewer = new LaravelLogViewer();
+        $this->request = app('request');
     }
 
     /**
-     * @return \Illuminate\Http\RedirectResponse|\Symfony\Component\HttpFoundation\BinaryFileResponse
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Foundation\Application|\Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector|\Illuminate\View\View|\Symfony\Component\HttpFoundation\BinaryFileResponse
      */
     public function index()
     {
         if (request()->get('l')) {
-            LaravelLogViewer::setFile(base64_decode(request()->get('l')));
+            $this->log_viewer->setFile(base64_decode(request()->get('l')));
         }
 
-        if (request()->get('dl'))
-            return \Response::download(base64_decode(\Input::get('dl')));
+        if (request()->get('dl')) {
+            return response()->download(base64_decode($this->request->input('dl')));
+        }
 
         if (request()->has('del')) {
-            \File::delete(base64_decode(request()->get('del')));
-            return \Redirect::to(\Request::url());
+            app('files')->delete(base64_decode(request()->get('del')));
+
+            return redirect($this->request->url());
         }
 
         return view('maxham-log-viewer::log', [
             'logs' => $this->log_viewer->all(),
             'files' => $this->log_viewer->getFiles(true),
-            'current_file' => $this->log_viewer->getFileName()
+            'current_file' => $this->log_viewer->getFileName(),
         ]);
     }
 }
